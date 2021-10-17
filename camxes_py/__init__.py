6f0053758490302a315d9ee96fa9fd551d349728
Robin Lee Powell <rlpowell@digitalkingdom.org>
2021-10-16 23:44:20 -0700

    Various minor fixes
    - added match
    - give access to the parsimonious node for parse and match
    - get vlatai.py installed by pip
    - docs update

--- camxes_py/__init__.py
+++ camxes_py/__init__.py
@@ -2,11 +2,27 @@ from .parsers import camxes_ilmen
 from .transformers import camxes_json
 
 
-def parse(text, parser=None, rule=None, transformer=None):
+def parse(text, parser=None, rule=None, transformer=None, give_node=False):
     if parser is None:
         parser = camxes_ilmen.Parser(rule)
     if transformer is None:
         transformer = camxes_json.Transformer()
     parsed = parser.parse(text)
     transformed = transformer.transform(parsed)
-    return transformed
+    if give_node:
+        return transformed, parsed
+    else:
+        return transformed
+
+def match(text, parser=None, rule=None, transformer=None, give_node=False):
+    if parser is None:
+        parser = camxes_ilmen.Parser(rule)
+    if transformer is None:
+        transformer = camxes_json.Transformer()
+    parsed = parser.match(text)
+    transformed = transformer.transform(parsed)
+
+    if give_node:
+        return transformed, parsed
+    else:
+        return transformed
